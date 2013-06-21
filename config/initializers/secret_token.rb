61c37e2f85b3091b8ffc32dadfaecb9840d82aa8
arabi <mailtoraoof@gmail.com>
2013-06-21 22:06:22 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TinyTwitter::Application.config.secret_key_base = 'e301087343311c53fe1067e6cd8f6d6d046b6b1071e5f4c00c1fad41173b29d13c1c8a0ef9dddbff1097b5e389020be7c98ea962fd523ab5c028334692ac10f9'
